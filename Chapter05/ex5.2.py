9ad7b2a92ee092dc5c2c2dca9057eb26258c0bc4
Sandeep Sadarangani <sadarangani@sadarangani-macbookpro.roam.corp.google.com>
2018-12-03 16:51:26 +1100

    Working Files

---
+++ Chapter05/ex5.2.py
@@ -0,0 +1,27 @@
+# Sandeep Sadarangani
+# Fermats Theorem check
+
+def check_fermat(a,b,c,n):
+    
+    result = 0
+    
+    calc = a**n + b**n
+    calc_test = c**n
+    
+    if(calc == calc_test):
+        result = 1
+    
+    return result
+    
+
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+c = int(input("Enter value for c: "))
+n = int(input("Enter value for n: "))
+
+result = check_fermat(a,b,c,n)
+
+if(result == 1):
+    print("Fermat was wrong!")
+else:
+    print("No, that doesn't work")
