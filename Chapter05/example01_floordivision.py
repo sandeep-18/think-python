a484b886a35746afa24c90bb8d1747faa672121e
Sandeep Sadarangani <sadarangani@sadarangani-macbookpro.roam.corp.google.com>
2018-12-03 14:08:54 +1100

    Floor Division Example

---
+++ Chapter05/example01_floordivision.py
@@ -0,0 +1,13 @@
+# Sandeep Sadarangani - 3/12/18
+# Testing the difference between floor division (integer division) and regular division in 
+# Python3
+
+minutes = 105
+
+# Floor Division - cuts the float part
+floor_division_result = minutes//60
+print(floor_division_result)
+
+# Regular Division - keeps the float part
+regular_division_result = minutes/60
+print(regular_division_result)
